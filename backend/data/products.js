11f5d36d3256e276daa1dc0dbe167ec8ea37e67b
Jamal Rizki <jamalbrizki@gmail.com>
2023-05-03 18:56:10 -0400

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,149 @@
+const products = [
+  {
+    name: '100',
+    image: '/images/100.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Aja',
+    image: '/images/aja.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cambambera',
+    image: '/images/Cambambera.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Champeta',
+    image: '/images/Champeta.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cogela',
+    image: '/images/cogela.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Fritos',
+    image: '/images/Fritos.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Fundingue',
+    image: '/images/Fundingue.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Gato',
+    image: '/images/gato.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'God',
+    image: '/images/GOD.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pechiche',
+    image: '/images/PECHICHE.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pupy',
+    image: '/images/pupy.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Valores',
+    image: '/images/valores.png',
+    description:
+      '100% Sustainable Cotton',
+    brand: 'TDL',
+    category: 'T-shirts',
+    price: 30,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  },
+  
+]
+
+export default products
